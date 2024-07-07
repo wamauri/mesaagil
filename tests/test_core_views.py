43a6b9640a9684ebb1accd5afcc232623becb5a6
Amauri Clementino <amaurisantospro@gmail.com>
2024-07-06 21:55:04 -0300

    Test views

--- tests/test_core_views.py
+++ tests/test_core_views.py
@@ -3,8 +3,25 @@ from django.urls import reverse
 from django.test import Client
 
 
-def test_home_view_return_200():
-    c = Client()
-    rev = reverse('home')
-    res = c.get(rev)
-    assert res.status_code == 200
+class TestViews:
+    def setup_method(self, test_method):
+        self.c = Client()
+
+    def teardown_method(self, test_method):
+        pass
+
+    def test_home_view_return_200(self):
+        url = reverse('home')
+        res = self.c.get(url)
+        assert res.status_code == 200
+
+    def test_overview_view_return_200(self):
+        url = reverse('overview')
+        res = self.c.get(url)
+        assert res.status_code == 200
+
+    def test_dashboard_view_return_200(self):
+        url = reverse('dashboard')
+        res = self.c.get(url)
+        print(dir(res))
+        assert res.status_code == 200
