df968208abb997ce7a05d8e6a29347e7274a8730
Amauri Clementino <amaurisantospro@gmail.com>
2024-07-14 18:05:18 -0300

    Organize apps

--- core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path(route='', view=views.home, name='home'),
-    path(route='accounts/login/', view=views.login_view, name='login'),
-    path(route='accounts/logout/', view=views.logout_view, name='logout'),
-]
