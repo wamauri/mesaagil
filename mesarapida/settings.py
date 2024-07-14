d3b4a39287ecb5e9d1e03e7c5a38b62241db5bb6
Amauri Clementino <amaurisantospro@gmail.com>
2024-07-14 11:17:14 -0300

    Setting messages, session, and login process

--- mesarapida/settings.py
+++ mesarapida/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+from django.contrib import messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,3 +133,16 @@ AUTH_USER_MODEL = 'core.CustomUser'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = 'accounts/login/'
+
+MESSAGE_STORAGE = "django.contrib.messages.storage.cookie.CookieStorage"
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-secondary',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger'
+}
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
+SESSION_COOKIE_AGE = (604800 * 3)  # 3 weeks in seconds
--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,26 @@
 from django.conf import settings
+from django.http import HttpRequest
 
 
-def redirect_user(request):
+def build_next_path(request: HttpRequest) -> str:
+    '''Builds the next path for the non-logged user'''
     return f"{settings.LOGIN_URL}?next={request.path}"
+
+
+def get_next_path(request: HttpRequest) -> str:
+    '''Gets the next path to redirect the user'''
+    try:
+        return request.GET['next']
+    except:
+        return 'home'
+
+
+def get_remeber(request: HttpRequest) -> str | None:
+    '''
+    Gets the remember value if it exists 
+    otherwise return None
+    '''
+    try:
+        return request.POST['remember']
+    except:
+        return None
